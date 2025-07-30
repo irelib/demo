eac2a28dee905e4b93ae6e75b0e4423af22db9fe
程灼 <cjy1085703408@gmail.com>
2025-07-30 09:30:44 +0800

    feat:

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="PageContainer">
+    <div class="PageContent"></div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.PageContainer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  height: 100%;
+
+  .PageContent {
+    height: 100vh;
+  }
+}
+</style>
