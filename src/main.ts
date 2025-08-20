ebc5e7e74bec4e9fad040f65217f2cd388147755
程灼 <cjy1085703408@gmail.com>
2025-08-20 11:37:39 +0800

    feat:

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
-import { createApp } from "vue";
-import { router } from "@/router";
-import "reset-css";
-import "@/styles/reset.scss";
-import "@/styles/index.scss";
-import "@/styles/variable.scss";
-import "tdesign-vue-next/es/style/index.css";
-import "overlayscrollbars/overlayscrollbars.css";
-import App from "./App.vue";
+import { createApp } from 'vue';
+import { router } from '@/router';
+import '@/styles/reset.scss';
+import '@/styles/index.scss';
+import '@/styles/variable.scss';
+import 'tdesign-vue-next/es/style/index.css';
+import 'overlayscrollbars/overlayscrollbars.css';
+import App from './App.vue';
 
 const app = createApp(App);
 
 app.use(router);
-app.mount("#app");
+app.mount('#app');
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,27 @@
-import { createRouter, createWebHistory, RouteLocationNormalized } from "vue-router";
-import { baseRoutes, RouteRecordRawExtra } from "@/router/routes.ts";
-import NProgress from "nprogress";
-import "nprogress/nprogress.css";
+import { createRouter, createWebHashHistory, RouteLocationNormalized } from 'vue-router';
+import { baseRoutes, RouteRecordRawExtra } from '@/router/routes.ts';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 
 type RouteGuardParamsType = RouteLocationNormalized & RouteRecordRawExtra;
 
 NProgress.configure({ showSpinner: false });
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [...baseRoutes],
+	history: createWebHashHistory(import.meta.env.BASE_URL),
+	routes: [...baseRoutes],
 });
 
 router.beforeEach(async (to: RouteGuardParamsType, _from: RouteGuardParamsType, next) => {
-  document.title = <string>to.meta?.title || "Demo";
+	document.title = <string>to.meta?.title || 'Demo';
 
-  NProgress.start();
+	NProgress.start();
 
-  next();
+	next();
 });
 
 router.afterEach(() => {
-  NProgress.done();
+	NProgress.done();
 });
 
 export { router };
